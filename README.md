c5009f41647a4e11bbe6f4b7204b9924b97ceb09
khalil estell <khalil.estell@sjsu.edu>
2014-10-09 15:25:30 -0700

    Removing unnecessary deprecated files

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 Scape.js
 ========
 
-A very small framework for single page websites that dynamically load HTML and JavaScript.
+A very small framework for building single page websites that dynamically loads HTML and JavaScript files for you. It also wraps JQuery AJAX calls to get rid of XSS and CSRF and to validate server information. 
 
 Dependencies
 ========
 
 JQuery 2.x
+Underscore.JS 1.7.0
--- compress.sh
+++ compress.sh
@@ -1,24 +1,16 @@
 #! /bin/bash
 
-echo -e "\e[1m\e[36mCompiling ScapeJS\e[0m"
+echo -e "Compiling ScapeJS"
 
-BOLD="$(tput bold)"
-DELL="$(tput cub 100)"
-DEFAULT="$(tput sgr 0)"
-
-echo -en "\e[31m$BOLD[=      ] 1/7 'src/generator.js'               $DELL"
-java -jar yuicompressor-2.4.7.jar --type js "src/generator.js" >> Scape.js
-
-echo -en "\e[31m$BOLD[==     ] 2/7 'src/communicator.js'               $DELL"
-java -jar yuicompressor-2.4.7.jar --type js "src/ajax-communicator.js" >> Scape.js
-
-echo -en "\e[31m$BOLD[====== ] 6/8 'src/router.js'               $DELL"
+echo "[=  ] 2/3 'src/router.js'"
 java -jar yuicompressor-2.4.7.jar --type js "src/router.js" >> Scape.js
 
-echo -e  "\e[31m$BOLD[=======] 7/7 Finishing              $DELL"
+echo "[== ] 1/3 'src/communicator.js'"
+java -jar yuicompressor-2.4.7.jar --type js "src/communicator.js" >> Scape.js
+
+echo "[===] 3/3 Finishing"
 java -jar yuicompressor-2.4.7.jar --type js "src/Scape.js" >> Scape.js
 
 mv Scape.js dist/Scape.min.js
 
-echo -e '\e[1m\e[32mDone!\e[0m'
-echo -en "$DEFAULT"
+echo 'Done!'
